f40ac584ca6d436af7204b56c843ea546b32614e
Eloi Chouvet <eloi@elois-mbp.home>
2018-06-07 16:31:16 +0200

    c plus jolie comme sa

--- login.js
+++ login.js
@@ -1,33 +1,32 @@
 if (req.body.login && req.body.pass)
-    {
-                 sql = 'SELECT * FROM `users` WHERE login = ?';
-                 variables = [req.body.login];
-                 con.query(sql, variables,
-                 function (err, result) { 
-                    if (err) throw err;
-                    if (result.length > 0)
-                    {
-                        console.log(result[0].confirm);
-                        
-                            bcrypt.compare(req.body.pass, result[0].pass, function(err, reso) {
-                            if (reso)
-                            {
-                                if (result[0].confirm === 1)
-                                {
-                                    res.render('login.ejs', {css: css, success: 'BRAVO TU ES CONNECTÉ !'});
-                                }
-                                else
-                                    res.render('login.ejs', {css: css, error: 'Veuillez confirmer votre compte par email'});
-                            }
-                            else
-                                res.render('login.ejs', {css: css, error: 'ERREUR DE CONNEXION ! (invalid pass)'});
-                            });
-                    }
-                    else
-                    {
-                        res.render('login.ejs', {css: css, error: 'ERREUR DE CONNEXION ! (login inconnu)'});
-                    }
-                });
-    }
+{
+    sql = 'SELECT * FROM `users` WHERE login = ?';
+    variables = [req.body.login];
+    con.query(sql, variables,
+    function (err, result) { if (err) throw err;
+       if (result.length > 0)
+       {
+           console.log(result[0].confirm);
+           bcrypt.compare(req.body.pass, result[0].pass, function(err, reso) 
+           {
+               if (reso)
+               {
+                   if (result[0].confirm === 1)
+                   {
+                       res.render('login.ejs', {css: css, success: 'BRAVO TU ES CONNECTÉ !'});
+                   }
+                   else
+                       res.render('login.ejs', {css: css, error: 'Veuillez confirmer votre compte par email'});
+               }
+               else
+                   res.render('login.ejs', {css: css, error: 'ERREUR DE CONNEXION ! (invalid pass)'});
+           });
+       }
+       else
+       {
+           res.render('login.ejs', {css: css, error: 'ERREUR DE CONNEXION ! (login inconnu)'});
+       }
+    });
+}
 else
     res.render('login.ejs', {css: css, error: 'Filling in Every field is required'});
